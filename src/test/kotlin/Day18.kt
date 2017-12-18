122b7c87e2823dfe7ef30d1d96bfe9773bec9af2
Christof Vollrath <christof@taobits.net>
2017-12-19 00:45:39 +0100

    Day18/1

---
+++ src/test/kotlin/Day18.kt
@@ -0,0 +1,353 @@
+import org.amshove.kluent.`should equal`
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
+
+/*
+--- Day 18: Duet ---
+You discover a tablet containing some strange assembly code labeled simply "Duet".
+Rather than bother the sound card with it, you decide to run the code yourself.
+Unfortunately, you don't see any documentation, so you're left to figure out what the instructions mean on your own.
+
+It seems like the assembly is meant to operate on a set of registers
+that are each named with a single letter and that can each hold a single integer.
+You suppose each register should start with a value of 0.
+
+There aren't that many instructions, so it shouldn't be hard to figure out what they do.
+Here's what you determine:
+
+snd X plays a sound with a frequency equal to the value of X.
+set X Y sets register X to the value of Y.
+add X Y increases register X by the value of Y.
+mul X Y sets register X to the result of multiplying the value contained in register X by the value of Y.
+mod X Y sets register X to the remainder of dividing the value contained in register X by the value of Y
+(that is, it sets X to the result of X modulo Y).
+rcv X recovers the frequency of the last sound played, but only when the value of X is not zero.
+(If it is zero, the command does nothing.)
+jgz X Y jumps with an offset of the value of Y, but only if the value of X is greater than zero.
+(An offset of 2 skips the next instruction, an offset of -1 jumps to the previous instruction, and so on.)
+Many of the instructions can take either a register (a single letter) or a number.
+The value of a register is the integer it contains; the value of a number is that number.
+
+After each jump instruction, the program continues with the instruction to which the jump jumped.
+After any other instruction, the program continues with the next instruction.
+Continuing (or jumping) off either end of the program terminates it.
+
+For example:
+
+set a 1
+add a 2
+mul a a
+mod a 5
+snd a
+set a 0
+rcv a
+jgz a -1
+set a 1
+jgz a -2
+
+The first four instructions set a to 1, add 2 to it, square it, and then set it to itself modulo 5,
+resulting in a value of 4.
+Then, a sound with frequency 4 (the value of a) is played.
+After that, a is set to 0, causing the subsequent rcv and jgz instructions to both be skipped
+(rcv because a is 0, and jgz because a is not greater than 0).
+Finally, a is set to 1, causing the next jgz instruction to activate,
+jumping back two instructions to another jump, which jumps again to the rcv,
+which ultimately triggers the recover operation.
+At the time the recover operation is executed, the frequency of the last sound played is 4.
+
+What is the value of the recovered frequency (the value of the most recently played sound)
+the first time a rcv instruction is executed with a non-zero value?
+
+ */
+
+class Day18Spec : Spek({
+    describe("duet") {
+        on("example input") {
+            it("should have last sound played 4") {
+                val instructions = parseDuetInstructions(day18ExampleInput)
+                Duet(instructions).execute().sound `should equal` 4L
+            }
+        }
+    }
+
+    describe("instruction parameter") {
+        on("const parameter") {
+            val duet = Duet()
+            it("should have value") {
+                DuetConst(7).value(duet) `should equal` 7L
+            }
+        }
+        on("register parameter") {
+            val duet = Duet(registers = mapOf('a' to 5L))
+            it("should have value of register") {
+                DuetRegister('a').value(duet) `should equal` 5L
+            }
+        }
+    }
+    describe("duet instructions") {
+        on("set") {
+            val duet = Duet()
+            it("should set register a from value") {
+                val result = DuetSet('a', DuetConst(2)).execute(duet)
+                result.registers['a'] `should equal` 2L
+                result.pc `should equal` 1
+            }
+            it("should set register b from register a") {
+                val duetWithA = DuetSet('a', DuetConst(3)).execute(duet)
+                val result = DuetSet('b', DuetRegister('a')).execute(duetWithA)
+                result.registers['b'] `should equal` 3L
+                result.pc `should equal` 2
+            }
+            it("should set register b from register a when a is empty") {
+                DuetSet('b', DuetRegister('a')).execute(duet).registers['b'] `should equal` 0L
+            }
+        }
+        on("add") {
+            val duet = Duet()
+            val duetWithA = DuetSet('a', DuetConst(2)).execute(duet)
+            it("should add to register a witch is empty") {
+                DuetAdd('a', DuetConst(3)).execute(duet).registers['a'] `should equal` 3L
+            }
+            it("should add to register a witch contains 2") {
+                val result = DuetAdd('a', DuetConst(3)).execute(duetWithA)
+                result.registers['a'] `should equal` 5L
+                result.pc `should equal` 2
+            }
+            it("should add a to register b") {
+                DuetAdd('b', DuetRegister('a')).execute(duetWithA).registers['a'] `should equal` 2L
+            }
+            it("should add empty a to register b") {
+                DuetAdd('b', DuetRegister('a')).execute(duet).registers['b'] `should equal` 0L
+            }
+        }
+        on("mul") {
+            val duet = Duet()
+            val duetWithA = DuetSet('a', DuetConst(2)).execute(duet)
+            it("should multiply register a and value") {
+                DuetMul('a', DuetConst(3)).execute(duetWithA).registers['a'] `should equal` 6L
+            }
+        }
+        on("mod") {
+            val duet = Duet()
+            val duetWithA = DuetSet('a', DuetConst(5)).execute(duet)
+            it("should calculate register a mod value") {
+                DuetMod('a', DuetConst(3)).execute(duetWithA).registers['a'] `should equal` 2L
+            }
+        }
+        on("snd") {
+            val duet = Duet()
+            val duetWithA = DuetSet('a', DuetConst(5)).execute(duet)
+            it("should sound frequency") {
+                DuetSnd(DuetConst(4)).execute(duet).sound `should equal` 4L
+            }
+            it("should sound frequenc from register") {
+                DuetSnd(DuetRegister('a')).execute(duetWithA).sound `should equal` 5L
+            }
+        }
+        on("rcv") {
+            val duet = Duet()
+            it("should stop when non zero value") {
+                DuetRcv(DuetConst(1)).execute(duet).stop `should equal` true
+            }
+            it("should not stop when zero value") {
+                DuetRcv(DuetConst(0)).execute(duet).stop `should equal` false
+            }
+        }
+        on("jgz") {
+            val duet = Duet()
+            it("should jump when non zero value") {
+                DuetJgz(DuetConst(1), DuetConst(3)).execute(duet).pc `should equal` 3
+            }
+            it("should not jump when zero value") {
+                DuetJgz(DuetConst(0), DuetConst(3)).execute(duet).pc `should equal` 1
+            }
+        }
+    }
+    describe("parse instructions") {
+        on("example input") {
+            val input = day18ExampleInput
+            it("should parse to the correct instructions") {
+                parseDuetInstructions(input) `should equal` listOf(
+                        DuetSet('a', DuetConst(1)),
+                        DuetAdd('a', DuetConst(2)),
+                        DuetMul('a', DuetRegister('a')),
+                        DuetMod('a', DuetConst(5)),
+                        DuetSnd(DuetRegister('a')),
+                        DuetSet('a', DuetConst(0)),
+                        DuetRcv(DuetRegister('a')),
+                        DuetJgz(DuetRegister('a'), DuetConst(-1)),
+                        DuetSet('a', DuetConst(1)),
+                        DuetJgz(DuetRegister('a'), DuetConst(-2))
+                )
+            }
+        }
+    }
+    describe("duet part 1") {
+        on("exercise input") {
+            val instructions = parseDuetInstructions(day18Input)
+            val received = Duet(instructions).execute().sound
+            println("sound received: $received")
+        }
+    }
+
+})
+
+data class Duet(val instructions: List<DuetInstr> = listOf(),
+                val registers: Map<Char, Long> = mapOf(),
+                val sound: Long = 0,
+                val pc: Int = 0,
+                val stop: Boolean = false) {
+    fun execute(): Duet =
+        if (stop) this
+        else {
+            val instruction = instructions[pc]
+            println("pc: $pc instr: $instruction sound: $sound registers: $registers")
+            instruction.execute(this).execute()
+        }
+}
+
+abstract class DuetInstr() {
+    abstract fun execute(duet: Duet): Duet
+}
+data class DuetSet(val r: Char, val i: DuetParam) : DuetInstr() {
+    override fun execute(duet: Duet): Duet {
+        val changedRegisters = duet.registers.toMutableMap()
+        changedRegisters.set(r, i.value(duet))
+        return duet.copy(registers = changedRegisters, pc = duet.pc + 1)
+    }
+}
+data class DuetAdd(val r: Char, val i: DuetParam) : DuetInstr() {
+    override fun execute(duet: Duet): Duet {
+        val changedRegisters = duet.registers.toMutableMap()
+        val changedReg = (duet.registers[r]?:0) + i.value(duet)
+        changedRegisters.set(r, changedReg)
+        return duet.copy(registers = changedRegisters, pc = duet.pc + 1)
+    }
+}
+data class DuetMul(val r: Char, val i: DuetParam) : DuetInstr() {
+    override fun execute(duet: Duet): Duet {
+        val changedRegisters = duet.registers.toMutableMap()
+        val changedReg = (duet.registers[r]?:0) * i.value(duet)
+        changedRegisters.set(r, changedReg)
+        return duet.copy(registers = changedRegisters, pc = duet.pc + 1)
+    }
+}
+data class DuetMod(val r: Char, val i: DuetParam) : DuetInstr() {
+    override fun execute(duet: Duet): Duet {
+        val changedRegisters = duet.registers.toMutableMap()
+        val changedReg = (duet.registers[r]?:0) % i.value(duet)
+        changedRegisters.set(r, changedReg)
+        return duet.copy(registers = changedRegisters, pc = duet.pc + 1)
+    }
+}
+data class DuetSnd(val i: DuetParam) : DuetInstr() {
+    override fun execute(duet: Duet) = duet.copy(sound = i.value(duet), pc = duet.pc + 1)
+}
+data class DuetRcv(val i: DuetParam) : DuetInstr() {
+    override fun execute(duet: Duet) = with (i.value(duet)) {
+        if (this == 0L) duet.copy(pc = duet.pc + 1)
+        else duet.copy(stop = true)
+    }
+}
+data class DuetJgz(val r: DuetParam, val i: DuetParam) : DuetInstr() {
+    override fun execute(duet: Duet) = with (r.value(duet)) {
+        if (this == 0L) duet.copy(pc = duet.pc + 1)
+        else duet.copy(pc = duet.pc + i.value(duet).toInt())
+    }
+}
+
+abstract class DuetParam() {
+    abstract fun value(duet: Duet): Long
+}
+data class DuetConst(val v: Long) : DuetParam() {
+    override fun value(duet: Duet) = v
+}
+data class DuetRegister(val r: Char) : DuetParam() {
+    override fun value(duet: Duet) = duet.registers[r]?:0
+}
+
+fun parseDuetInstructions(input: String) =
+        input.split("\n")
+        .mapIndexed { index, s ->  Pair(index, s)}
+        .filter { ! it.second.isBlank() }
+                .map {
+                    val parts = it.second.trim().split("""\s+""".toRegex())
+                    val cmd = parts[0]
+                    val par1 = parts[1]
+                    val par2 = if (parts.size >= 3) parts[2] else null
+                    when(cmd) {
+                        "set" -> DuetSet(parseRegister(par1), parseParamenter(par2!!))
+                        "add" -> DuetAdd(parseRegister(par1), parseParamenter(par2!!))
+                        "mul" -> DuetMul(parseRegister(par1), parseParamenter(par2!!))
+                        "mod" -> DuetMod(parseRegister(par1), parseParamenter(par2!!))
+                        "snd" -> DuetSnd(parseParamenter(par1))
+                        "rcv" -> DuetRcv(parseParamenter(par1))
+                        "jgz" -> DuetJgz(parseParamenter(par1), parseParamenter(par2!!))
+                        else -> throw IllegalArgumentException("Cmd: $cmd illegal, line ${it.first}")
+                    }
+                }
+
+fun parseParamenter(par: String): DuetParam =
+        if (par[0].isLetter()) DuetRegister(par[0])
+        else DuetConst(par.toLong())
+fun parseRegister(par: String): Char = par[0]
+
+
+val day18ExampleInput = """
+        set a 1
+        add a 2
+        mul a a
+        mod a 5
+        snd a
+        set a 0
+        rcv a
+        jgz a -1
+        set a 1
+        jgz a -2
+        """
+
+val day18Input = """
+        set i 31
+        set a 1
+        mul p 17
+        jgz p p
+        mul a 2
+        add i -1
+        jgz i -2
+        add a -1
+        set i 127
+        set p 735
+        mul p 8505
+        mod p a
+        mul p 129749
+        add p 12345
+        mod p a
+        set b p
+        mod b 10000
+        snd b
+        add i -1
+        jgz i -9
+        jgz a 3
+        rcv b
+        jgz b -1
+        set f 0
+        set i 126
+        rcv a
+        rcv b
+        set p a
+        mul p -1
+        add p b
+        jgz p 4
+        snd a
+        set a b
+        jgz 1 3
+        snd b
+        set f 1
+        add i -1
+        jgz i -11
+        snd a
+        jgz f -16
+        jgz a -19
+        """
